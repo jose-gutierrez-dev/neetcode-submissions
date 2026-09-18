662fe019d0b835bbd783f314f874f14340e6470e
jose-gutierrez-dev <gutierrez.jose.dev@gmail.com>
2026-09-18 01:39:05 -0400

    Add: valid-palindrome-ii - submission-6

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-6.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        l_stor, r_stor = None, None
+        skipped = 0
+        while l < r:
+            if s[l] != s[r]:
+                skipped += 1
+                if skipped == 2:
+                    break
+                l_stor = l
+                r_stor = r
+                l += 1
+                continue
+            l += 1
+            r -= 1
+
+        if skipped != 2:
+            return True
+
+        l, r = l_stor, r_stor - 1
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+
+        return True
+
+        
+        

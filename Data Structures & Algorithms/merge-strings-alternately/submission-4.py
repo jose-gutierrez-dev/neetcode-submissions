0e5042bf359b9e996a2ae80eae94daa023d6bc1f
jose-gutierrez-dev <gutierrez.jose.dev@gmail.com>
2026-09-18 02:01:05 -0400

    Add: merge-strings-alternately - submission-4

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merged_words = ""
+        op = 0
+        tp = 0
+        len_1 = len(word1)
+        len_2 = len(word2)
+        while True:
+            if op < len_1:
+                merged_words += word1[op]
+                op += 1
+            else:
+                return merged_words + word2[tp:]
+            if tp < len_2:
+                merged_words += word2[tp]
+                tp += 1
+            else:
+                return merged_words + word1[op:]
